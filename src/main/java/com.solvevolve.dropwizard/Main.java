cbfb719905f3f741813b038d450b8817bbdc92bc
durga.p <durga.p@flipkart.com>
2016-06-07 09:19:32 +0530

    Hello World

---
+++ src/main/java/com.solvevolve.dropwizard/Main.java
@@ -0,0 +1,11 @@
+package com.solvevolve.dropwizard;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    System.out.println("Hello world");
+    System.out.println("Command line Arguments are " + String.join(",", args));
+
+  }
+}
